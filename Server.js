3de1c6b861ad1b95e30f57d3d0eb95d4ab47885c
danidan902 <danid93575@gmail.com>
2025-07-26 11:40:47 -0700

    Update Server.js

--- Server.js
+++ Server.js
@@ -10,7 +10,12 @@ dotenv.config();
 connectDB();
 
 const app = express();
-app.use(cors());
+app.use(cors(
+  app.use(cors(
+  origin: 'https://danievent.vercel.app', 
+  methods: ['GET', 'POST', 'PUT', 'DELETE'],
+  credentials: true
+));
 app.use(express.json())
 
 app.use('/api/auth', authRoutes);
